b9cf04c59ec00a1163378bcd7a79c446d44fe324
xVyrusz <cisnnerosbcrra@hotmail.com>
2022-03-13 15:59:51 -0600

    Clase 7 Terminada

--- index.js
+++ index.js
@@ -10,12 +10,33 @@ app.get('/home', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  res.json({
+app.get('/doctors', (req, res) => {
+  res.json([{
     name: 'Producto 1',
     price: 1000
-  });
+  }, {
+    name: 'Producto 2',
+    price: 2000
+  }]);
 });
+
+app.get('/doctors/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
+  res.json({
+    id,
+    name: 'Producto 2',
+    price: 2000
+  })
+});
+
+app.get('/patients/:patientsId', (req, res) => {
+  const { patientsId } = req.params;
+  res.json({
+    patientsId
+  })
+})
 
 app.listen(port, () => {
   console.log('Mi port' + port);
